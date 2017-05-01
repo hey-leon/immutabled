e039e188673c16055e06f352d9b5d96c88270769
Leon Pearce <leonp1991@gmail.com>
2017-05-01 10:34:00 +1000

    TEST: add size checks to immut.from

--- tests/immut/from.spec.js
+++ tests/immut/from.spec.js
@@ -20,12 +20,12 @@ import {
 import { _ } from '../../source/utils/curry'
 
 
-test('immut#from: should be curryable', t => {
+test('immut.from: should be curryable', t => {
   t.is(parse(_), parse)
 })
 
 
-test('immut#from: should parse simple map', t => {
+test('immut.from: should parse simple map', t => {
   const shape = {
     type: map.type,
     keys: {
@@ -34,17 +34,13 @@ test('immut#from: should parse simple map', t => {
     }
   }
 
-  t.deepEqual(
-    parse(shape, { a: 'holla', b: 10 } ),
-    map.of({
-      a: 'holla',
-      b: 10,
-    })
-  )
+  const i = parse(shape, { a: 'holla', b: 10 } )
+  t.is(i.__size__, 2)
+  t.deepEqual(i, map.of({ a: 'holla', b: 10 }))
 })
 
 
-test('immut#from: should strip excess keys on map', t => {
+test('immut.from: should strip excess keys on map', t => {
   const shape = {
     type: map.type,
     keys: {
@@ -52,16 +48,13 @@ test('immut#from: should strip excess keys on map', t => {
     }
   }
 
-  t.deepEqual(
-    parse(shape, { a: 'holla', b: 33 }),
-    map.of({
-      b: 33,
-    })
-  )
+  const i = parse(shape, { a: 'holla', b: 33 })
+  t.is(i.__size__, 1)
+  t.deepEqual(i, map.of({ b: 33 }))
 })
 
 
-test('immut#from: should add defaults for missing keys', t => {
+test('immut.from: should add defaults for missing keys', t => {
   const shape = {
     type: map.type,
     keys: {
@@ -70,34 +63,22 @@ test('immut#from: should add defaults for missing keys', t => {
     }
   }
 
-  t.deepEqual(
-    parse(shape, { b: 22 }),
-    map.of({
-      b: 22,
-      c: 'yolo',
-    })
-  )
+  const i = parse(shape, { b: 22 })
+  t.is(i.__size__, 2)
+  t.deepEqual(i, map.of({ b: 22, c: 'yolo' }))
 })
 
 
-test('immut#from: should parse simple list', t => {
-  const shape = {
-    type: list.type,
-    item: 0
-  }
+test('immut.from: should parse simple list', t => {
+  const shape = { type: list.type }
 
-  t.deepEqual(
-    parse(shape, [ 10, 22, 11 ]),
-    list.of([
-      10,
-      22,
-      11,
-    ])
-  )
+  const i = parse(shape, [ 10, 22, 11 ])
+  t.is(i.__size__, 3)
+  t.deepEqual(i, list.of([ 10, 22, 11 ]))
 })
 
 
-test('immut#from: should parse map of maps', t => {
+test('immut.from: should parse map of maps', t => {
   const shape = {
     type: map.type,
     keys: {
@@ -112,20 +93,20 @@ test('immut#from: should parse map of maps', t => {
     },
   }
 
-  t.deepEqual(
-    parse(shape, { b: 1, c: { b: "compose" } }),
-    map.of({
-      b: 1,
-      c: map.of({
-        a: "kubernetes",
-        b: "compose",
-      }),
-    })
-  )
+  const i = parse(shape, { b: 1, c: { b: "compose" } })
+  t.is(i.__size__, 2)
+  t.is(i.__data__.c.__size__, 2)
+  t.deepEqual(i, map.of({
+    b: 1,
+    c: map.of({
+      a: "kubernetes",
+      b: "compose",
+    }),
+  }))
 })
 
 
-test('immut#from: should parse list of lists', t => {
+test('immut.from: should parse list of lists', t => {
   const shape = {
     type: list.type,
     item: {
@@ -134,41 +115,44 @@ test('immut#from: should parse list of lists', t => {
     },
   }
 
-  t.deepEqual(
-    parse(shape, [ [], [], [], [] ]),
-    list.of([
-      list.of(),
-      list.of(),
-      list.of(),
-      list.of(),
-    ])
-  )
+  const i = parse(shape, [ [], [], [], [] ])
+  t.is(i.__size__, 4)
+  t.is(i.__data__[0].__size__, 0)
+  t.is(i.__data__[1].__size__, 0)
+  t.is(i.__data__[2].__size__, 0)
+  t.is(i.__data__[3].__size__, 0)
+  t.deepEqual(i, list.of([
+    list.of(),
+    list.of(),
+    list.of(),
+    list.of(),
+  ]))
 })
 
 
-test('immut#from: should parse complex shape', t => {
+test('immut.from: should parse complex shape', t => {
   const shape = {
     type: list.type,
     item: {
       type: map.type,
       keys: {
         b: 33,
-        c: {
-          type: list.type,
-          item: 0,
-        },
+        c: { type: list.type },
       },
     },
   }
 
-  t.deepEqual(
-    parse(shape, [ { b: 22 }, { c: [ 10, 11 ] }, {} ]),
-    list.of([
-      map.of({ b: 22, c: list.of() }),
-      map.of({ b: 33, c: list.of([ 10, 11 ]) }),
-      map.of({ b: 33, c: list.of() }),
-    ])
-  )
+  const i = parse(shape, [ { b: 22 }, { c: [ 10, 11 ] }, {} ])
+  t.is(i.__size__, 3)
+  t.is(i.__data__[0].__size__, 2)
+  t.is(i.__data__[1].__size__, 2)
+  t.is(i.__data__[2].__size__, 2)
+  t.is(i.__data__[1].__data__.c.__size__, 2)
+  t.deepEqual(i, list.of([
+    map.of({ b: 22, c: list.of() }),
+    map.of({ b: 33, c: list.of([ 10, 11 ]) }),
+    map.of({ b: 33, c: list.of() }),
+  ]))
 })
 
 
@@ -182,7 +166,7 @@ test('ofList: should throw if d !== []', t => {
 })
 
 
-test('#ofList: should always return data if exists', t => {
+test('ofLeaf: should always return data if exists', t => {
   const d1 = 'holla world'
   const d2 = null
 
